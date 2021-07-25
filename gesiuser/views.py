6791eb704ea57e04a0f7ddca64a62d9813823926
gil_duck <godud1118@naver.com>
2021-07-25 14:34:06 +0900

    Django project

--- gesiuser/views.py
+++ gesiuser/views.py
@@ -2,7 +2,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Gsuser
-
+from .forms import LoginForm
 
 def home(request):
     user_id = request.session.get('user')
@@ -20,27 +20,8 @@ def logout(request):
     return redirect('/')
 
 def login(request):
-    if request.method == 'GET':
-        return render(request,'login.html')
-    
-    elif request.method =='POST':
-        username = request.POST.get('username',None)
-        password = request.POST.get('password',None)
-
-        res_data = {}
-        if not(username and password):
-            res_data['error'] = "모든 값을 입력해주세요"
-        else:
-            gsuser = Gsuser.objects.get(username = username)
-            if check_password(password, gsuser.password):
-                # 비밀번호가 일치 (로그인처리)
-                request.session['user'] = gsuser.id
-
-                return redirect('/')
-            else: 
-                res_data['error'] = "비밀번호가 틀렸습니다."
-                
-        return render(request,'login.html',res_data)
+    form = LoginForm()
+    return render(request,'login.html', {'form': form })
     
 
 def register(request):
